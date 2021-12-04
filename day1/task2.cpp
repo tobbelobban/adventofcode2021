936b9e75f291c214d97380449db131dd21ea2a2a
fred <tobyedwards@hotmail.co.uk>
2021-12-04 16:36:46 +0100

    Done day 1

--- day1/task2.cpp
+++ day1/task2.cpp
@@ -5,12 +5,10 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     unsigned int counter = 0;
-    int prev_depth, depth, next_depth, next_next_depth, sum;
+    int prev_depth, depth, next_depth, next_next_depth;
     cin >> prev_depth >> depth >> next_depth;
-    sum = depth + next_depth;
     while(cin >> next_next_depth) {
-        if(sum + next_next_depth > sum + prev_depth) ++counter;
-        sum = sum - depth + next_next_depth;
+        if(next_next_depth > prev_depth) ++counter;
         prev_depth = depth;
         depth = next_depth;
         next_depth = next_next_depth;
