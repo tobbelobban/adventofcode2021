bae97c49491a769e5504bde7cdb8ebc01b20b298
fred <tobyedwards@hotmail.co.uk>
2021-12-07 19:18:25 +0100

    Done day 7

--- day7/task2.cpp
+++ day7/task2.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 #include<unordered_map>
-#include<vector>
 #include<numeric>
 #include<cmath>
 #include<algorithm>
+#include<chrono>
 
 using namespace std;
 
@@ -13,24 +13,25 @@ int fuel_cost(const int& dist) {
 
 int main(int argc, char const *argv[])
 {   
+    auto start = chrono::steady_clock::now();
     unordered_map<int,int> m;
-    vector<int> v;
     string next_pos;
-    int val, init_guess = 0;
+    int val, init_guess = 0, n = 0;
     while(getline(cin,next_pos,',')) {
         val = stoi(next_pos);
         init_guess += val;
-        v.push_back(val);
         if(m.find(val) != m.end()) {
             ++m[val];
         } else {
             m.insert({val,1});
         }
+        ++n;
     }
-    sort(v.begin(), v.end());
     int cost = 0;
-    init_guess = floor((double)init_guess/v.size());
+    init_guess = floor((double)init_guess/n);
     for(auto iter = m.begin(); iter != m.end(); ++iter) cost += fuel_cost(abs(iter->first - init_guess)) * iter->second;
+    auto end = chrono::steady_clock::now();
+    cout << "Time: " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms" << endl;
     std::cout << cost << endl;
     return 0;
 }
